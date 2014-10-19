88aa429f682d3c87c6a8b2a011bb51745198abe2
xiaodan lu <luxiandanegg@gmail.com>
2014-10-19 13:18:56 -0700

    home page finished

--- workshop/.build/js/app.js
+++ workshop/.build/js/app.js
@@ -14,70 +14,10 @@ require(['jquery','bootstrap'], function ($,bootstrap) {
     var app = {
         initialize: function () {
             
-            $(document).ready(function(){
-            var lastClickId;
-            $(".slideDown").click(function(){
-
-                if($("#panel").is(":visible")){
-                    if(this.id==lastClickId){
-                        $("#panel").fadeOut("fast");
-                    }
-                    else{
-                        $("#panel").fadeOut("fast");
-                        switch (this.id) {
-                            case "courses":
-                                $("#panel").html('{>"courses"/}');
-                                break;
-                            case "school":
-                                $("#panel").html('{>"school"/}');
-                                break;
-                            case "about":
-                                $("#panel").html('{>"about"/}');
-                                break;
-                            case "pages":
-                                $("#panel").html('{>"pages"/}');
-                                break;
-                            case "contacts":
-                                $("#panel").html('{>"contacts"/}');
-                                break;
-                            default: 
-                                $("#panel").html(this.id);
-                                break; 
-                        }
-                        $("#panel").slideToggle("600");
-                    }
-                    
-                }
-                else{
-                    switch (this.id) {
-                        case "courses":
-                            $("#panel").html('{>"courses"/}');
-                            break;
-                        case "school":
-                            $("#panel").html('{>"school"/}');
-                            break;
-                        case "about":
-                            $("#panel").html('{>"about"/}');
-                            break;
-                        case "pages":
-                            $("#panel").html('{>"pages"/}');
-                            break;
-                        case "contacts":
-                            $("#panel").html('{>"contacts"/}');
-                            break;
-                        default: 
-                            $("#panel").html(this.id);
-                            break; 
-                    }
-                    $("#panel").slideToggle("600");
-                }
-            lastClickId = this.id;
-          });
-        });
+            
         }
     };
 
     app.initialize();
 
 });
-
